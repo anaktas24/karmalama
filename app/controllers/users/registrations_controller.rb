0147255797e81d4ee8e3f31f53c1bc29364b9f0e
Ioannis Antoniou <johnantony24@hotmail.com>
2023-06-05 19:42:41 +0200

    login

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,10 +9,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
       @user = User.new
       render 'step1'
     when 2
-      @user = User.new(session[:user_step1_params].merge(user_params)) if session[:user_step1_params]
+      @user = User.new(session[:user_step1_params]&.merge(user_params) || {})
       render 'step2'
     when 3
-      # Code for step 3
+      @user = User.new(session[:user_step1_params].merge(session[:user_step2_params])) if session[:user_step1_params] && session[:user_step2_params]
+      render 'step3'
     else
       redirect_to new_user_registration_path(user: { step: 1 })
     end
@@ -26,36 +27,42 @@ class Users::RegistrationsController < Devise::RegistrationsController
       session[:user_step1_params] = user_params
       redirect_to new_user_registration_path(user: { step: 2 })
     when 2
-      @user = User.new(user_params)
-      if session[:user_step1_params].present?
-        @user.assign_attributes(session[:user_step1_params])
-      end
-      if @user.valid?
-        if @user.save
-          session[:user_step1_params] = nil
-          redirect_to new_user_registration_path(user: { step: 3 })
-        else
-          puts @user.errors.full_messages # Add this line
-          render 'step2'
-        end
+      @user = User.new(session[:user_step1_params].merge(user_params)) if session[:user_step1_params]
+
+      if @user && @user.valid?
+        session[:user_step2_params] = user_params
+        redirect_to new_user_registration_path(user: { step: 3 })
       else
-        puts @user.errors.full_messages # Add this line
+        @user ||= User.new
         render 'step2'
       end
     when 3
-      # Code for step 3
-      redirect_to root_path
+      @user = User.new(session[:user_step1_params].merge(session[:user_step2_params])) if session[:user_step1_params] && session[:user_step2_params]
+
+      if @user && @user.valid?
+        if @user.save
+          session[:user_step1_params] = nil
+          session[:user_step2_params] = nil
+          redirect_to root_path
+        else
+          render 'step3'
+        end
+      else
+        @user ||= User.new
+        render 'step3'
+      end
     else
       redirect_to new_user_registration_path(user: { step: 1 })
     end
   end
 
-
-
-
   private
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :name, :surname, :phone, :birthday, :postal, :area, :step])
+  end
+
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name, :surname, :phone, :birthday, :postal, :area)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :surname, :phone, :birthday, :postal, :area, :step)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,14 @@ Rails.application.routes.draw do
   patch '/profile', to: 'pages#update_profile'
   get '/my_assignments', to: 'pages#my_assignments'
 
+  get '/users/step1', to: 'users/registrations#step1', as: 'step1_user_registration'
+  post '/users/step1', to: 'users/registrations#process_step1', as: 'process_step1_user_registration'
+  # Replace the existing routes for step 2
+  get '/users/step2', to: 'users/registrations#step2', as: 'step2_user_registration'
+  post '/users/step2', to: 'users/registrations#process_step2', as: 'process_step2_user_registration'
 
+  get '/users/step3', to: 'users/registrations#step3', as: 'step3_user_registration'
+  post '/users/step3', to: 'users/registrations#process_step3', as: 'process_step3_user_registration'
 
 
 
