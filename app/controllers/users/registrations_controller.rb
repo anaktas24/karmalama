9086c11a28f5a8774e88dbe8e9955ad73eb87cc9
Ioannis Antoniou <johnantony24@hotmail.com>
2023-06-09 19:09:20 +0200

    fixed signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,12 +12,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
       @user = User.new(session[:user_step1_params]&.merge(user_params) || {})
       render 'step2'
     when 3
-      @user = User.new(session[:user_step1_params].merge(session[:user_step2_params])) if session[:user_step1_params] && session[:user_step2_params]
-      render 'step3'
-    else
-      redirect_to new_user_registration_path(user: { step: 1 })
+      if session[:user_step1_params].is_a?(Hash) && session[:user_step2_params].is_a?(Hash)
+        @user = User.new(session[:user_step1_params].merge(session[:user_step2_params].except("picture"))) if session[:user_step1_params] && session[:user_step2_params]
+        render 'step3'
+      else
+        redirect_to new_user_registration_path(user: { step: 1 }) and return
+      end
     end
   end
+
 
   def create
     @step = params.dig(:user, :step).to_i || 1
@@ -29,33 +32,40 @@ class Users::RegistrationsController < Devise::RegistrationsController
     when 2
       @user = User.new(session[:user_step1_params].merge(user_params)) if session[:user_step1_params]
 
-      if @user && @user.valid? && params[:user][:picture].present?
-        @user.picture.attach(params[:user][:picture])
-
+      if @user && @user.valid?
         session[:user_step2_params] = user_params
         redirect_to new_user_registration_path(user: { step: 3 }) and return
       else
         @user ||= User.new
-        flash.now[:alert] = "Please provide a picture."
+        flash.now[:alert] = "Please provide valid information."
         render 'step2'
       end
     when 3
-      @user = User.new(session[:user_step1_params].merge(session[:user_step2_params])) if session[:user_step1_params] && session[:user_step2_params]
+      if session[:user_step1_params].is_a?(Hash) && session[:user_step2_params].is_a?(Hash)
+        @user = User.new(session[:user_step1_params].merge(session[:user_step2_params])) if session[:user_step1_params] && session[:user_step2_params]
 
-      if @user && @user.valid?
-        @user.save
-        sign_in(@user)
-        session[:user_step1_params] = nil
-        session[:user_step2_params] = nil
-        flash[:notice] = "Registration completed successfully!"
-        redirect_to root_path and return
-      else
-        render 'step3'
+        if @user && @user.valid?
+          @user.save
+          session[:user_step1_params] = nil
+          session[:user_step2_params] = nil
+
+          sign_in(@user)
+          flash[:notice] = "Registration completed successfully!"
+          redirect_to root_path and return
+        end
       end
+
+      # If user creation fails or session data is missing, render step3
+      @user ||= User.new
+      render 'step3'
     else
       redirect_to new_user_registration_path(user: { step: 1 }) and return
     end
   end
+
+
+
+
 
   private
 
