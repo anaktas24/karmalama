def33a2a77878e439cd414f008f8d81be9b04ef9
Ioannis Antoniou <johnantony24@hotmail.com>
2023-06-16 21:42:21 +0200

    card lisitng

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    if user_signed_in?
+    # if user_signed_in?
       # @default_radius = 50
       # @listings = Listing.near([current_user.latitude, current_user.longitude], @default_radius, order: 'distance').limit(24)
     # else
       @listings = Listing.all.order('created_at DESC').limit(24)
-    end
+    # end
 
     if params[:query].present?
       @listings = @listings.where(category_type: params[:query])
@@ -17,12 +17,6 @@ class PagesController < ApplicationController
       @listings = @listings.order(params[:order_by] => params[:order])
     end
   end
-
-
-
-
-
-
 
   private
 
