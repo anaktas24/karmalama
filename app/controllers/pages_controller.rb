b6722c31c78444ea271c9e21ae3606f8d88a29c8
Ioannis Antoniou <johnantony24@hotmail.com>
2023-06-16 20:43:01 +0200

    card lisitng

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,30 +1,24 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :home
 
   def home
     if user_signed_in?
-      @default_radius = 50
-      @listings = Listing.near([current_user.latitude, current_user.longitude], @default_radius, order: 'distance')
-    else
-      @listings = Listing.all.order('created_at DESC')
+      # @default_radius = 50
+      # @listings = Listing.near([current_user.latitude, current_user.longitude], @default_radius, order: 'distance').limit(24)
+    # else
+      @listings = Listing.all.order('created_at DESC').limit(24)
     end
 
     if params[:query].present?
       @listings = @listings.where(category_type: params[:query])
     end
 
-    if params[:order_by].present? && params[:order].present?
-      order_column = params[:order_by] == 'distance' ? 'distance' : params[:order_by]
-      order_direction = params[:order] == 'asc' ? 'ASC' : 'DESC'
-      @listings = @listings.order(Arel.sql("#{order_column} #{order_direction}"))
+    if params[:order_by].present?
+      @listings = @listings.order(params[:order_by] => params[:order])
     end
-
-    @listings = @listings.limit(24) # Limit the number of listings to display
-
-    puts "Listing Query: #{@listings.to_sql}"
-
-    # Other code in the action
   end
+
+
 
 
 
